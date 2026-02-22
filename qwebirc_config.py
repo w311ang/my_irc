8de78de332c335cb1cb46fa1acb0db51afeb45b9
w311ang <30381083+w311ang@users.noreply.github.com>
2026-02-22 21:10:42 +0800

    Update qwebirc_config.py

--- qwebirc_config.py
+++ qwebirc_config.py
@@ -3,3 +3,8 @@ REALIRCSERVER = ('irc-backend', 6667)
 
 BASE_URL = 'http://0.0.0.0:9090'
 NETWORK_NAME = 'My Retro IRC'
+
+STATIC_BASE_URL = ''
+APP_TITLE = 'My Retro IRC'
+THEME = 'default'
+DYNAMIC_CONFIG = {}
